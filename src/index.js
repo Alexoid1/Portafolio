bba573dbaa2af2408390a5b5de5474540d9343fa
Alexoid1 <alex_zam@hotmail.es>
2020-11-18 00:57:37 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 
 const app=express();
-
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
