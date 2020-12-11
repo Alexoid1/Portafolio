558dd0d1bf1a4e4b3aa2615c3c71cabbc7010a97
Alexoid1 <alex_zam@hotmail.es>
2020-12-11 13:16:03 -0500

    add link to spanish resume

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ leng.addEventListener('click',()=>{
     if(le==='en'){
         leng.innerHTML='ES  <i class="fa fa-caret-down" aria-hidden="true"></i>'
         le='es';
-        downloadResume.href=''
+        downloadResume.href='https://drive.google.com/u/0/uc?id=1AkmvRXZhdDcH_Kef47EDaHN4un4gbjjS&export=download';
 
     }else {
         leng.innerHTML='EN  <i class="fa fa-caret-down" aria-hidden="true"></i>'
         le='en'
-        downloadResume.href='https://drive.google.com/u/0/uc?id=1PrrVdhM-YF7zorg10wRDWf9VbNybQn92&export=download'
+        downloadResume.href='https://drive.google.com/u/0/uc?id=1PrrVdhM-YF7zorg10wRDWf9VbNybQn92&export=download';
     }
 })
