56f5f7065124ee0becd7e59549e8feb431410ec1
Alexoid1 <alex_zam@hotmail.es>
2020-12-10 22:51:25 -0500

    add button

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const botonSend = document.getElementById('botonSend');
 const formContact = document.getElementById('formContact');
+const leng = document.getElementById('leng');
+
+leng.addEventListener('click',()=>{
+    
+})
