86d78aafbfbbfb60a4857ea5530de2d4310010f2
Alexoid1 <alex_zam@hotmail.es>
2020-12-11 13:23:02 -0500

    fix eslint

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const botonSend = document.getElementById('botonSend');
-const formContact = document.getElementById('formContact');
 const leng = document.getElementById('leng');
 const downloadResume = document.getElementById('downloadResume');
-var le='en'
-leng.addEventListener('click',()=>{
-    if(le==='en'){
-        leng.innerHTML='ES  <i class="fa fa-caret-down" aria-hidden="true"></i>'
-        le='es';
-        downloadResume.href='https://drive.google.com/u/0/uc?id=1AkmvRXZhdDcH_Kef47EDaHN4un4gbjjS&export=download';
-
-    }else {
-        leng.innerHTML='EN  <i class="fa fa-caret-down" aria-hidden="true"></i>'
-        le='en'
-        downloadResume.href='https://drive.google.com/u/0/uc?id=1PrrVdhM-YF7zorg10wRDWf9VbNybQn92&export=download';
-    }
-})
+let le = 'en';
+leng.addEventListener('click', () => {
+  if (le === 'en') {
+    leng.innerHTML = 'ES  <i class="fa fa-caret-down" aria-hidden="true"></i>';
+    le = 'es';
+    downloadResume.href = 'https://drive.google.com/u/0/uc?id=1AkmvRXZhdDcH_Kef47EDaHN4un4gbjjS&export=download';
+  } else {
+    leng.innerHTML = 'EN  <i class="fa fa-caret-down" aria-hidden="true"></i>';
+    le = 'en';
+    downloadResume.href = 'https://drive.google.com/u/0/uc?id=1PrrVdhM-YF7zorg10wRDWf9VbNybQn92&export=download';
+  }
+});
