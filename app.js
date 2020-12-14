f3d97d2a6ef23c10bb41a8e440e82a4978fb74f8
Alexoid1 <alex_zam@hotmail.es>
2020-12-13 21:23:12 -0500

    add img selection buttons

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const leng = document.getElementById('leng');
 const downloadResume = document.getElementById('downloadResume');
+const roboGif = document.getElementById('roboGif');
+const p1i1=document.getElementById('p1i1').addEventListener('click', img1);
+const p1i2=document.getElementById('p1i2').addEventListener('click', img2);
+const p1i3=document.getElementById('p1i3').addEventListener('click', img3);
+
 let le = 'en';
 leng.addEventListener('click', () => {
   if (le === 'en') {
@@ -12,6 +17,18 @@ leng.addEventListener('click', () => {
     downloadResume.href = 'https://drive.google.com/u/0/uc?id=1PrrVdhM-YF7zorg10wRDWf9VbNybQn92&export=download';
   }
 });
+
+function img1(){
+  roboGif.src='./image/img1.png'
+}
+
+function img2(){
+  roboGif.src='./image/img2.png'
+}
+
+function img3(){
+  roboGif.src='./image/robogif.gif'
+}
 
 const TypeWriter = function (txtEle,words,wait=3000) {
   this.txtEle=txtEle;
@@ -38,20 +55,23 @@ TypeWriter.prototype.type=function(){
   }
   this.txtEle.innerHTML=`<span class="cur">${this.text}</span>`;
 
-  let typeSpeed=300;
+  let typeSpeed=80;
   if(this.isDeleting){
     typeSpeed /=2;
   }
 
   if(!this.isDeleting&&this.text===fullText){
     typeSpeed=this.wait
-    this.isDeleting=true;
+    setTimeout(()=>{ this.isDeleting=true;},3000)
+   
 
+  }else if (this.isDeleting && this.text==='') {
+    this.isDeleting=false;
   }
 
 
   console.log(fullText);
-  setTimeout(()=>this.type(),500)
+  setTimeout(()=>this.type(),80)
 };
 
 document.addEventListener('DOMContentLoaded', init);
